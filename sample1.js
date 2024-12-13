ace56b6311dbaf1e89b3fb6b9ada87222ad63d8a
Karthik-S-L-21 <karthik.sl@codeandtheory.com>
2024-12-13 11:09:22 +0530

    fix: minor changes.

--- sample1.js
+++ sample1.js
@@ -1,8 +1,4 @@
 console.log("Hello World..");
 
 console.log("Testing creating pr1");
-<<<<<<< HEAD
-console.log("Testing creating pr2");
-=======
 console.log("cherry pick 1");
->>>>>>> parent of a0cea7a (fix: minor change.)
