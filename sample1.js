4a254f1d5bcd3942e3336d6280ac6e72e2c4708b
Karthik-S-L-21 <karthik.sl@codeandtheory.com>
2024-12-13 11:07:10 +0530

    fix: before revert commit 1.

--- sample1.js
+++ sample1.js
@@ -1,8 +1,3 @@
 console.log("Hello World..");
 
-console.log("Testing creating pr1");
-<<<<<<< HEAD
-console.log("Testing creating pr2");
-=======
-console.log("cherry pick 1");
->>>>>>> parent of a0cea7a (fix: minor change.)
+console.log("Before revert commit 1.");
