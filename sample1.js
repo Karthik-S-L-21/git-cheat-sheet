b677135b577391528ecd23e7291f15c34a38cf34
Karthik-S-L-21 <karthik.sl@codeandtheory.com>
2024-12-13 11:08:35 +0530

    Revert "fix: before revert commit 1."
    This reverts commit 4a254f1d5bcd3942e3336d6280ac6e72e2c4708b.

--- sample1.js
+++ sample1.js
@@ -1,3 +1,8 @@
 console.log("Hello World..");
 
-console.log("Before revert commit 2.");
+console.log("Testing creating pr1");
+<<<<<<< HEAD
+console.log("Testing creating pr2");
+=======
+console.log("cherry pick 1");
+>>>>>>> parent of a0cea7a (fix: minor change.)
